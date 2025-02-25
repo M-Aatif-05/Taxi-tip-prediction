04d8757223a70360dd93b7618e542ba704e95009
M-Aatif-05 <161297991+M-Aatif-05@users.noreply.github.com>
2025-02-26 00:50:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,12 @@ st.title("🚕 LGBM Tip Prediction App")
 # User Inputs
 passenger_count = st.number_input("Passenger Count")
 trip_distance = st.number_input("Trip Distance (miles)")
-fare_amount = st.number_input("Fare Amount ($)")
 day_night = st.selectbox("Day or Night", [0, 1])  # 0 = Day, 1 = Night
-trip_duration = st.number_input("Trip Duration (minutes)")
+trip_duration_minutes = st.number_input("Trip Duration (minutes)")
 
 # Prediction Button
 if st.button("Predict Tip 💰"):
-    input_features = np.array([[passenger_count, trip_distance, fare_amount, day_night, trip_duration]])
+    input_features = np.array([[passenger_count, trip_distance, day_night, trip_duration_minutes]])
     prediction = model.predict(input_features)[0]
 
     
