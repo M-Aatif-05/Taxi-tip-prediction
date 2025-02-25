8011f47a0ea32df1a517a8e786ce12a10c278a8d
M-Aatif-05 <161297991+M-Aatif-05@users.noreply.github.com>
2025-02-26 00:32:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ trip_duration = st.number_input("Trip Duration (minutes)")
 
 # Prediction Button
 if st.button("Predict Tip 💰"):
-    input_features = pd.DataFrame([[passenger_count, trip_distance, fare_amount, day_night, trip_duration]])
+    input_features = np.array([[passenger_count, trip_distance, fare_amount, day_night, trip_duration]])
     prediction = model.predict(input_features)
     
     st.success(f"💵 Predicted Tip Amount: ${prediction:.2f}")
